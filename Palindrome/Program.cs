ac754643f0e25301b720035d0ab7dd649a92e406
mukerem <mukeremali112@gmail.com>
2023-09-08 16:57:50 +0300

    create a function that check palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Palindrome
+{
+    public static bool isPalindrome(string input)
+    {
+        input = input.ToLower();
+
+        char[] punctuation = new char[] { ' ', '.', ',', ';', '!', '?', ':', '-', '_', '(', ')', '[', ']', '{', '}', '\'', '\"' };
+        foreach (var punct in punctuation)
+        {
+            input = input.Replace(punct.ToString(), "");
+        }
+
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        string reverseString = new string(charArray);
+
+        return input == reverseString;
+    }
+
+    public static void Main(string[] args)
+    {
+        string testString = "Racecar.";
+        bool ispal = isPalindrome(testString);
+
+        if(ispal)
+            Console.WriteLine($"{testString} is a palindrome");
+        else
+            Console.WriteLine($"{testString} is not a palindrome");
+    }
+}
+
